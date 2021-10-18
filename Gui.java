9b688faf53f5471e420f8620e2f426ec5bc45db5
Sanjeev Easparan <44382134+GalaxyCrusher23@users.noreply.github.com>
2021-10-18 22:17:51 +0000

    added a "play" button, does not do anything though

--- Gui.java
+++ Gui.java
@@ -30,24 +30,36 @@ public class Gui{
     return panel;
   }
 
-  public static JLabel tag(JPanel panel, String text, int x, int y, int fontSize) {
-    JLabel label = new JLabel(text, SwingConstants.CENTER);
-    label.setLocation(x, y);
+  public static JLabel tag(JPanel panel, String text, int x, int y, int width, int height, int fontSize) {
+    JLabel label = new JLabel(text);
+    label.setBounds(x, y, width, height);
     label.setFont(new Font("Serif", Font.BOLD, fontSize));
     panel.add(label);
     return label;
+  }
+
+  public static JButton button(JPanel panel, String text, int x, int y, int width, int height){
+    JButton button = new JButton(text);
+    button.setBounds(x, y, width, height);
+    panel.add(button);
+    //button.addActionListener(new GUI());
+    return button;
   }
 
   
 
   public static void start(){
     startMenu = panel(window, windowWidth, windowLength);
+    startMenu.setLayout(null);
 
     startMenu.setBackground(new Color(0, 125, 0));
 
-    JLabel title = tag(startMenu, "Blackjack", (int)windowWidth/2, 10, 19);
-
+    JLabel title = tag(startMenu, "Blackjack", windowWidth/2-50, 0, 200, 100, 19);
     title.setForeground(Color.WHITE);
+
+    JButton play = button(startMenu, "Play", windowWidth/2, windowLength/2, 100, 100);
+    play.setBackground(Color.YELLOW);
+    play.setForeground(Color.BLACK);
 
     startMenu.setVisible(true);
   }
