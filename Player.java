279d6267bf595c5ea71ae2f8596a734acbca4911
Sanjeev Easparan <44382134+GalaxyCrusher23@users.noreply.github.com>
2021-10-19 02:36:49 +0000

    added a constructor for the Player Class

--- Player.java
+++ Player.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class Player{
   public int [] playerHand = {0,0,0,0,0};
   private int playerTotal = 0;
+  private int money;
+
+  public Player(int money){
+    this.money = money;
+  }
 
   public int getplayerCards(total) {
     playerTotal += total + Cards.getcards();
